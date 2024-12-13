1a2520761251e8d4a2228abfa6efbce4631075cf
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2024-12-13 23:06:36 +0530

    added composition in js tutorial

---
+++ Composition_in_JS/script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b
+}
+
+function square(val) {
+    return val * val
+}
+
+// Now if we want to use above function sequentially
+// i.e, a function that sequentially performs first addition and then square of the addition value!
+// in this case, it's better to use composition
+
+function composeTwoFunction(fn1, fn2) {
+    return function (a, b) {
+        return fn2(fn1(a, b))
+    }
+}
+
+const task = composeTwoFunction(add, square)
+console.log(task(2,3))
